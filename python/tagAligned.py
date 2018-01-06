cc8d4b92cf91e1af2fba1885674e43f445294e23
Robert Henderson <balamkej@gmail.com>
2018-01-06 08:06:57 -0800

    fixed base directories

--- python/tagAligned.py
+++ python/tagAligned.py
@@ -3,39 +3,41 @@ from os import walk, listdir
 from os.path import isfile, join
 import re
 
-alignedDir = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/For_analysis/"
-expDir = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/"
+annotedDir = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/For_analysis/"
+alignedDir = "/Users/balamkej/Dropbox/andrea_usp_work/Forced_alignment/Resample/5000_epochs_QA_trained_only/"
 
-allAlignedFiles = [f for f in listdir(alignedDir) if re.search(r'TextGrid', f)]
-allExpFolders = [f for f in listdir(expDir) if re.search(r'S0', f)]
+# allAlignedFiles = [f for f in listdir(alignedDir) if re.search(r'TextGrid', f)]
+# allExpFolders = [f for f in listdir(expDir) if re.search(r'S0', f)]
 
-numberOfSpk = 5
+# numberOfSpk = 5
 
-alignedFilesBySpeaker = []
-for i in range(1,numberOfSpk+1):
-    speaker = 'S' + str(i).zfill(2)
-    speakerFiles = [f for f in allAlignedFiles if re.search(speaker, f)]
-    alignedFilesBySpeaker.append(speakerFiles)
+# alignedFilesBySpeaker = []
+# for i in range(1,numberOfSpk+1):
+#     speaker = 'S' + str(i).zfill(2)
+#     speakerFiles = [f for f in allAlignedFiles if re.search(speaker, f)]
+#     alignedFilesBySpeaker.append(speakerFiles)
 
-rawFilesBySpeakers = []
-for i in allExpFolders:
-    directory = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/" + i
-    rawFilesBySpeakers = rawFilesBySpeakers + [f for f in listdir(directory) if re.search(r'TextGrid', f)]
+# rawFilesBySpeakers = []
+# for i in allExpFolders:
+#     directory = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/" + i
+#     rawFilesBySpeakers = rawFilesBySpeakers + [f for f in listdir(directory) if re.search(r'TextGrid', f)]
 
-list.sort(allExpFolders)
-list.sort(alignedFilesBySpeaker)
+# list.sort(allExpFolders)
+# list.sort(alignedFilesBySpeaker)
 
-for i in alignedFilesBySpeaker:
-    sort(i)
+# for i in alignedFilesBySpeaker:
+#     sort(i)
 
-for i in range(numberOfSpk):
-    expFile = expDir + allExpFolders[i] + rawFilesBySpeakers[i]
-    alignedList = alignedFilesBySpeaker[i]
-    tg = tgt.read_textgrid(expFile)
-    annotations_tier = tg.get_tier_by_name('Annotations')
-    for j in range(annotations_tier):
-        alignedTextGrid = tgt.read_textgrid(alignedDir + alignedList[j])
-        annotation = annotations_tier[j].text
+# for i in range(numberOfSpk):
+#     expFile = expDir + allExpFolders[i] + rawFilesBySpeakers[i]
+#     alignedList = alignedFilesBySpeaker[i]
+#     tg = tgt.read_textgrid(expFile)
+#     annotations_tier = tg.get_tier_by_name('Annotations')
+#     for j in range(annotations_tier):
+#         alignedTextGrid = tgt.read_textgrid(alignedDir + alignedList[j])
+#         annotation = annotations_tier[j].text
+
+
 
 # A function that takes a textgrid and adds an annotation tier that matches
 # the annotation tier of an annotated textgrid. Note, the function assumes
@@ -55,6 +57,6 @@ def annotate(textGrid,annotatedTextGrid):
 
 
 
-expFile = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/" + "S01_Rosa_Petranilla" + "/" + "S01_Rosa_Petranilla_Mic1_OMNIDIR_Mic2_UNIDER.TextGrid"
-tg = tgt.read_textgrid(expFile)
-annotations_tier = tg.get_tier_by_name('Annotations')
+# expFile = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/" + "S01_Rosa_Petranilla" + "/" + "S01_Rosa_Petranilla_Mic1_OMNIDIR_Mic2_UNIDER.TextGrid"
+# tg = tgt.read_textgrid(expFile)
+# annotations_tier = tg.get_tier_by_name('Annotations')
