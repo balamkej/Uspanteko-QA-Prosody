83448ba15369916ae9a0032c1a7eff52ced113af
Robert Henderson <balamkej@gmail.com>
2018-01-08 15:27:52 -0800

    target items list and working directory

---
+++ python/annotateQuestions.py
@@ -0,0 +1,27 @@
+import tgt
+from os import walk, listdir
+from os.path import isfile, join
+import re
+
+annotatedDir = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/For_analysis/"
+
+items = [
+"jtéleb7",
+"xajab7",
+"suq7uk7",
+"cháaj",
+"jch7úuk7",
+"qálaq",
+"qapoop",
+"kaach7",
+"qajóoq7",
+"jkinaq7",
+"rixóqil",
+"qaxoot",
+"jcháaj",
+"rixóql",
+"kinaq7",
+"jkaach7",
+"jxajab7"
+]
+
