4a57c02aae77d319c240ccab430d34c6a411e1e5
Robert Henderson <balamkej@gmail.com>
2018-01-08 18:26:37 -0800

    fixed start_time / end_time requirement for adding tiers

--- python/annotateQuestions.py
+++ python/annotateQuestions.py
@@ -9,48 +9,31 @@ outDir = "/Users/balamkej/Dropbox/Uspanteko_NSF_project/Recordings/2017/For_anal
 allToAnnotate = [f for f in listdir(toAnnotateDir) if re.search(r'TextGrid', f)]
 textGrids = [[tgt.read_textgrid(toAnnotateDir + f), f] for f in allToAnnotate]
 
-items = [
-"jtéleb7",
-"xajab7",
-"suq7uk7",
-"cháaj",
-"jch7úuk7",
-"qálaq",
-"qapoop",
-"kaach7",
-"qajóoq7",
-"jkinaq7",
-"rixóqil",
-"qaxoot",
-"jcháaj",
-"rixóql",
-"kinaq7",
-"jkaach7",
-"jxajab7"
-]
 
 # Sort textgrids into those that elicit broad and narrow answers.
 broad = []
 narrow = []
 for i in textGrids:
+    st = i[0].start_time
+    et = i[0].end_time
     words = [f.text for f in i[0].tiers[1]] # Get words in the words tier
     if "NEN" in words:
-        annInterval = tgt.Interval(text="b-nen")
-        annTier = tgt.IntervalTier(name="Question Type")
+        annInterval = tgt.Interval(start_time=st, end_time=et, text="b-nen")
+        annTier = tgt.IntervalTier(start_time=st, end_time=et, name="Question Type")
         annTier.add_interval(annInterval)
         i[0].add_tier(annTier)
         broad.append(i)
     elif "LAMAS" in words:
-        annInterval = tgt.Interval(text="b-lamas")
-        annTier = tgt.IntervalTier(name="Question Type")
+        annInterval = tgt.Interval(start_time=st, end_time=et, text="b-lamas")
+        annTier = tgt.IntervalTier(start_time=st, end_time=et, name="Question Type")
         annTier.add_interval(annInterval)
         i[0].add_tier(annTier)
         broad.append(i)
     else:
         narrow.append(i)
-        
+
 for i in broad:
-    tgt.write_to_file(i, outDir + i[0] + '_annotated' + '.TextGrid', format='short')
+    tgt.write_to_file(i[0], outDir + i[1] + '_annotated' + '.TextGrid', format='short')
 
 
 
