dabc32e07a3b69cf3dfdf16d748e4cbafea80a14
Robert Henderson <balamkej@gmail.com>
2018-01-08 18:29:39 -0800

    switched broad for narrow

--- python/annotateQuestions.py
+++ python/annotateQuestions.py
@@ -18,19 +18,19 @@ for i in textGrids:
     et = i[0].end_time
     words = [f.text for f in i[0].tiers[1]] # Get words in the words tier
     if "NEN" in words:
-        annInterval = tgt.Interval(start_time=st, end_time=et, text="b-nen")
+        annInterval = tgt.Interval(start_time=st, end_time=et, text="n-nen")
         annTier = tgt.IntervalTier(start_time=st, end_time=et, name="Question Type")
         annTier.add_interval(annInterval)
         i[0].add_tier(annTier)
-        broad.append(i)
-    elif "LAMAS" in words:
-        annInterval = tgt.Interval(start_time=st, end_time=et, text="b-lamas")
-        annTier = tgt.IntervalTier(start_time=st, end_time=et, name="Question Type")
-        annTier.add_interval(annInterval)
-        i[0].add_tier(annTier)
-        broad.append(i)
-    else:
         narrow.append(i)
+    elif "LAMAS" in words:
+        annInterval = tgt.Interval(start_time=st, end_time=et, text="n-lamas")
+        annTier = tgt.IntervalTier(start_time=st, end_time=et, name="Question Type")
+        annTier.add_interval(annInterval)
+        i[0].add_tier(annTier)
+        narrow.append(i)
+    else:
+        broad.append(i)
 
 for i in broad:
     tgt.write_to_file(i[0], outDir + i[1] + '_annotated' + '.TextGrid', format='short')
